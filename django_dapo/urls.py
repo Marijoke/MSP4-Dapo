0bdbaf737bc320ee9f7fd845f8ed3f2de8d5cefe
Marijoke <mariam.adamu@hotmail.co.uk>
2021-09-26 15:02:07 +0000

    updated URLS to include allauth url inclusion

--- django_dapo/urls.py
+++ django_dapo/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from dapo.views import welcome_to_dapo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome_to_dapo, name='home')
+    path ('accounts', include('allauth.urls')), 
 ]
