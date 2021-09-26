589d7021fccf33afd63c826da1155b3a147960f9
Marijoke <mariam.adamu@hotmail.co.uk>
2021-09-26 10:55:33 +0000

    Secret key was exposed moved to env file. Python decouple to separate code and secure key

--- django_dapo/settings.py
+++ django_dapo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(e1%9)v2@q-9xj12e$#&(8kh^juf0%et%dtzt#ui@y_rh12n=e'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
