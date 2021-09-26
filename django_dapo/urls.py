0f1662d8e3fc0f6259ef6a7d7c75eb740722bec6
Marijoke <mariam.adamu@hotmail.co.uk>
2021-09-26 20:49:41 +0000

    home app functionality working. Import os required

--- django_dapo/urls.py
+++ django_dapo/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from dapo.views import welcome_to_dapo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome_to_dapo, name='home'),
     path('accounts/', include('allauth.urls')), 
+    path('', include('home.urls')),
 ]
