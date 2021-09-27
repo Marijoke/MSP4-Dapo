8f5c66f932829147c25b05f3fe1b95726770fc7b
Marijoke <mariam.adamu@hotmail.co.uk>
2021-09-27 23:19:56 +0000

    heaader and main page layout completed before testing

--- django_dapo/settings.py
+++ django_dapo/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'allauth.account', #basic account login and out and password resets
     'allauth.socialaccount', #allows login via social media
     'home',
-    
 ]
 
 MIDDLEWARE = [
@@ -148,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
