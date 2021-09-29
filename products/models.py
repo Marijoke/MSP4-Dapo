79204416c4757addd0fd54f1d91640e55c904f1e
Marijoke <mariam.adamu@hotmail.co.uk>
2021-09-29 11:32:11 +0000

    Product information completed inculding descriptions

--- products/models.py
+++ products/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    verbose_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+    def get_verbose_name(self):
+        return self.verbose_name
 
 
 class Product(models.Model):
