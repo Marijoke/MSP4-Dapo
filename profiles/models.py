4a952d9ce171095936cb7b41107e5a18fd275b48
Marijoke <mariam.adamu@hotmail.co.uk>
2021-10-24 05:43:39 +0000

    login styled using all auth base

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# from django_countries.fields import CountryField
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
@@ -21,7 +21,7 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, blank=True)
     default_postcode = models.CharField(max_length=20, blank=True)
 
-        # default_country = CountryField(blank_label='Country', blank=True)
+     default_country = CountryField(blank_label='Country', blank=True)
 
     def __str__(self):
         return self.user.username
