f89d92c2ecb73052342623d5750657e283ee7a7d
Marijoke <mariam.adamu@hotmail.co.uk>
2021-10-24 06:27:53 +0000

    updated all allauth files to allow style to be shared especially across sign in/up

--- profiles/models.py
+++ profiles/models.py
@@ -21,7 +21,7 @@ class UserProfile(models.Model):
     default_county = models.CharField(max_length=80, blank=True)
     default_postcode = models.CharField(max_length=20, blank=True)
 
-     default_country = CountryField(blank_label='Country', blank=True)
+    default_country = CountryField(blank_label='Country', blank=True)
 
     def __str__(self):
         return self.user.username
