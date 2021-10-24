c073fc57a0ce9e8a992df90fd34ed695c0f1546d
Marijoke <mariam.adamu@hotmail.co.uk>
2021-10-24 00:41:28 +0000

    checkout initial template and functionality

--- checkout/views.py
+++ checkout/views.py
@@ -16,7 +16,7 @@ from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
-import stripe
+# import stripe
 import json
 
 
@@ -106,14 +106,14 @@ def checkout(request):
                            "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
-        current_bag = bag_contents(request)
-        total = current_bag['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+      #  current_bag = bag_contents(request)
+       # total = current_bag['grand_total']
+        # stripe_total = round(total * 100)
+        # stripe.api_key = stripe_secret_key
+        # intent = stripe.PaymentIntent.create(
+          #  amount=stripe_total,
+           # currency=settings.STRIPE_CURRENCY,
+        # )
 
         # Attempt to prefill the form with any info
         # the user maintains in their profile
@@ -145,7 +145,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+     #   'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
