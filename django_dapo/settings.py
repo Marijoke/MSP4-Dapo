8fcbcca7e9cf691a9a00eb35c4f167779eb6d10c
Marijoke <mariam.adamu@hotmail.co.uk>
2022-01-06 20:51:25 +0000

    Heroku failure as well as superuser failure

--- django_dapo/settings.py
+++ django_dapo/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-(e1%9)v2@q-9xj12e$#&(8kh^juf0%et%dtzt#ui@y_rh12n=e'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
